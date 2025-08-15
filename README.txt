39042301003e29f093c08757478618ea16e6aca8
S.Muresan <s.muresan@spgs.org>
2025-08-15 10:49:02 +0100

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,10 @@
+Hello There! 
+This folder is no longer in use since 15/08/25
+The full static and templates folder as well as the main.py and requirements.txt files have been uploaded to a github repository.
+What is left to do in this folder to make a fully-fledged website:
+- Silverton Gallery Page
+- Wricklemarsh Gallery Page
+- Editing text throughout the Website 
+- Commercial Photos 
+- Team Photos
+- Our Expertise
---
+++ main.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template, request, redirect, flash
+import smtplib
+from email.message import EmailMessage
+import os
+
+app = Flask(__name__)
+app.secret_key = 'your_secret_key'
+
+EMAIL_ADDRESS = "goldsylveon1@gmail.com"
+EMAIL_PASSWORD = "qnuf xjen csho yixt"
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/services')
+def services():
+    return render_template('services.html')
+
+@app.route('/projects/queensgrove')
+def queensgrove():
+    return render_template('projects/queensgrove.html')
+
+@app.route('/projects/silverton')
+def silverton():
+    return render_template('projects/silverton.html')
+
+@app.route('/projects/wricklemarsh')
+def wricklemarsh():
+    return render_template('projects/wricklemarsh.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        name = request.form['name']
+        email = request.form['email']
+        message = request.form['message']
+
+        msg = EmailMessage()
+        msg['Subject'] = 'New Contact Form Submission'
+        msg['From'] = EMAIL_ADDRESS
+        msg['To'] = EMAIL_ADDRESS
+        msg.set_content(f"Name: {name}\nEmail: {email}\nMessage: {message}")
+
+        try:
+            with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+                smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+                smtp.send_message(msg)
+            flash("Message sent successfully!", "success")
+        except Exception as e:
+            flash("Failed to send message.", "danger")
+    return render_template('contact.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
